46f66fb1615a7779060c74b22e27737eb241d7e7
Jounnom <jounnom@gmail.com>
2026-07-27 19:19:27 +0900

    Update monthly_snapshot_job.py

--- monthly_snapshot_job.py
+++ monthly_snapshot_job.py
@@ -51,10 +51,10 @@ log = logging.getLogger("monthly_snapshot_job")
 KST = ZoneInfo("Asia/Seoul")
 
 OAUTH_SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive",
     "openid",
     "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/spreadsheets",
 ]
 
 # ============================================================
